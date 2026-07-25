b21c003c77f89237b34c878436af941452c83e77
sieni7 <sieni7@gmail.com>
2026-07-25 13:59:05 +0000

    Ajout dist/ dans .gitignore

--- akoris-cli/dist/generators/constitution.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function generateConstitution(): Promise<void>;
--- akoris-cli/dist/utils/files.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function ensureDir(dir: string): void;
-export declare function writeFile(filePath: string, content: string): void;
-export declare function fileExists(filePath: string): boolean;
-export declare function getAkorisDir(): string;
